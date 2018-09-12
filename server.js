75598fd2925f30a122d6c8404e978c426859b40b
Daniel Chen <team6612@gmail.com>
2018-09-12 15:35:31 +0800

    咕嚕靈波(●´∀`)ノ♡

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var firebase = require("firebase");
+const { Client } = require('pg');
 
-var firebaseConfig = require("./firebase-config")
-firebase.initializeApp(firebaseConfig);
-var db = firebase.firestore();
-db.settings({
-  timestampsInSnapshots: true
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
-var cntRef = db.collection('count').doc('count');
+
+client.connect();
 
 var port = process.env.PORT || 3000;
 server.listen(port);
@@ -22,24 +21,27 @@ app.get('/', function (req, res) {
 });
 
 var cnt = 0;
-cntRef.get().then((doc) => {
-  cnt = doc.data().cnt
+client.query('SELECT count FROM kururinpo;').then((res) => {
+  cnt = res.rows[0].count
   io.on('connection', async function (socket) {
     console.log('Client connaected, current cnt: '+cnt);
     socket.emit('first shot', cnt);
     socket.on('shoot', function (data) {
-      cntRef.set({ cnt: ++cnt });
-      io.sockets.emit('shot', cnt);
+      io.sockets.emit('shot', ++cnt);
     });
   });
   setInterval(async () => {
     const inc = Math.floor(Math.random()*10);
     for(let i = 0; i < inc; i++) {
-      cntRef.set({ cnt: ++cnt });
-      io.sockets.emit('shot', cnt);
+      io.sockets.emit('shot', ++cnt);
       await sleep(Math.floor(Math.random()*300));
     }
   }, 2000)
+  setInterval(() => {
+    client.query('UPDATE kururinpo SET count=$1;', [cnt], (err, res) => {
+      console.log(err ? err.stack : "UPDATE Success! Current cnt = "+cnt)
+    });
+  }, 10000)
 })
 
 function sleep(ms){
